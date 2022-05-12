1e90f38af2658758859500a00c096eb844d3a806
adaptive_o <victor.gulyaev@gmail.com>
2022-05-12 21:43:12 +0300

    the boolean order kata - constract main string

--- the_boolean_order/the_boolean_order_kata.py
+++ the_boolean_order/the_boolean_order_kata.py
@@ -1,3 +1,16 @@
-def solve(s,ops):
-    pass
+import itertools
 
+
+operators = ['&', '|', '^']
+
+
+def solve(s:str, ops:str):
+
+    string = ''.join( [x for t in itertools.zip_longest(s,ops,fillvalue='') for x in t])
+    print(string)
+    stack = []
+
+
+
+if __name__ == '__main__':
+    solve("ttftff","|&^&&")
