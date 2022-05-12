eed926599f656b143f8e62b914a5c2ad1a6615f7
Victor Gulyaev <viktor.gulyaev@raiffeisen.ru>
2022-05-12 17:00:03 +0300

    the millionth fibonacci kata solution with matrix multiplication

--- the_millionth_fibonacci_kata/kata_the_millionth_fibonacci_kata.py
+++ the_millionth_fibonacci_kata/kata_the_millionth_fibonacci_kata.py
@@ -1,14 +1,29 @@
-import numpy as np
+def multiplicate_matrix(matrix1:list, matrix2:list) -> list:
+    res = [[0] * len(matrix2[0]) for j in range(len(matrix2))]
+    for i in range(len(matrix1)):
+        for j in range(len(matrix2[0])):
+            for k in range(len(matrix1[0])):
+                res[i][j] = res[i][j] + matrix1[i][k]*matrix2[k][j]
+    return res
+
 
 def fib(n):
-    I = np.array([[1, 1], [1, 0]])
-    R = np.array([1, 0])
+    if n >= 0:
+        I = [[1, 1], [1, 0]]
+        k = 1
+    else:
+        I = [[0, -1], [-1, 1]]
+        n *= -1
+        k = 1 - 2*(n%2)
+    factor = n
+    f = [[0], [1]]
     while n>0:
         if n%2!=0:
-            R = R.dot(I)
-        I = I.dot(I)
-        n >>= 1 #Уменьшаем степень вдвое
-    return R[1]
+            f = multiplicate_matrix( I, f )
+        I = multiplicate_matrix(I, I)
+        n >>= 1
+    return (k**factor)*f[0][0]
+
 
 if __name__ == '__main__':
-    print(fib(1000000))
+    print(fib(-95))
