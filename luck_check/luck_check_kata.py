413dd7e03d5a9ab4502e5e6d576a1fcad54b1aa6
adaptive_o <victor.gulyaev@gmail.com>
2022-07-21 21:39:38 +0300

    luck check kata: version with "reduce"

--- luck_check/luck_check_kata.py
+++ luck_check/luck_check_kata.py
@@ -1,14 +1,11 @@
 import string
+from functools import reduce
+
 
 
 def luck_check(s):
     l_s = len(s)
     if (l_s == 0) or (len(set(s).difference(set(string.digits))) > 0):
         raise Exception("wrong input string")
-    if l_s % 2 == 1:
-        return False
-    sum_left, sum_right = 0, 0
-    for i in range(l_s // 2):
-        sum_left += int(s[i])
-        sum_right += int(s[l_s - 1 - i])
-    return sum_left == sum_right
+    m_s = l_s // 2
+    return reduce(lambda x, y: int(x)+int(y), s[:m_s]) == reduce(lambda x, y: int(x)+int(y), s[l_s - m_s:])
